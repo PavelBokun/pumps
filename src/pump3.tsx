4e3bb48a818c987adae0350a21b4d24b42097b51
PavelBokun <pavel.snabteh@gmail.com>
2024-05-20 23:21:30 +0300

    добавлен импр

--- src/pump3.tsx
+++ src/pump3.tsx
@@ -1,5 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
-import { RootState, increment } from "./store";
+import {useSelector } from "react-redux";
+import { RootState } from "./store";
 import { List } from "./package";
 
 export const Pumps3 = () => {
